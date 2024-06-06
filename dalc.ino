ee38c3a142ad8db376136e00cbe24d877ccd6bf0
14Mayank2004 <14mayank.khosa@gmail.com>
2024-06-06 17:29:55 +0530

    enhanced code

--- dalc.ino
+++ dalc.ino
@@ -5,8 +5,10 @@
 RTC_DS3231 rtc;
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 
-const int buttonSetTime = 2;   // Button to set time
-const int buttonSetAlarm = 3;  // Button to set alarm
+const int buttonSetTime = 2;   // Button to enter set time mode
+const int buttonIncrementHour = 4; // Button to increment hour
+const int buttonIncrementMinute = 5; // Button to increment minute
+const int buttonConfirm = 6; // Button to confirm the time setting
 const int buzzerPin = 8;
 
 bool alarmSet = false;
@@ -14,10 +16,12 @@ DateTime alarmTime;
 
 void setup() {
   pinMode(buttonSetTime, INPUT_PULLUP);
-  pinMode(buttonSetAlarm, INPUT_PULLUP);
+  pinMode(buttonIncrementHour, INPUT_PULLUP);
+  pinMode(buttonIncrementMinute, INPUT_PULLUP);
+  pinMode(buttonConfirm, INPUT_PULLUP);
   pinMode(buzzerPin, OUTPUT);
   digitalWrite(buzzerPin, LOW);
-  
+
   Wire.begin();
   rtc.begin();
   lcd.begin();
@@ -56,9 +60,10 @@ void loop() {
     setTime();
   }
 
-  if (digitalRead(buttonSetAlarm) == LOW) {
-    setAlarm();
-  }
+  // Placeholder for setting alarm
+  // if (digitalRead(buttonSetAlarm) == LOW) {
+  //   setAlarm();
+  // }
 
   if (alarmSet && now.hour() == alarmTime.hour() && now.minute() == alarmTime.minute()) {
     soundAlarm();
@@ -68,11 +73,44 @@ void loop() {
 }
 
 void setTime() {
-  // Add code to set the time using buttons
-}
+  int hour = 0;
+  int minute = 0;
+  bool settingTime = true;
 
-void setAlarm() {
-  // Add code to set the alarm using buttons
+  lcd.clear();
+  lcd.print("Set Time:");
+
+  while (settingTime) {
+    lcd.setCursor(0, 1);
+    lcd.print(hour < 10 ? "0" : "");
+    lcd.print(hour);
+    lcd.print(':');
+    lcd.print(minute < 10 ? "0" : "");
+    lcd.print(minute);
+
+    if (digitalRead(buttonIncrementHour) == LOW) {
+      delay(200); // debounce
+      hour++;
+      if (hour > 23) hour = 0;
+    }
+
+    if (digitalRead(buttonIncrementMinute) == LOW) {
+      delay(200); // debounce
+      minute++;
+      if (minute > 59) minute = 0;
+    }
+
+    if (digitalRead(buttonConfirm) == LOW) {
+      delay(200); // debounce
+      DateTime now = rtc.now();
+      rtc.adjust(DateTime(now.year(), now.month(), now.day(), hour, minute, 0));
+      settingTime = false;
+    }
+
+    delay(200);
+  }
+
+  lcd.clear();
 }
 
 void soundAlarm() {
